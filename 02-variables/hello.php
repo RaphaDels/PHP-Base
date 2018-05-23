c5fd448ded110715013db2f5281d6e61c31d8c8b
raphadels <raphaeledelsemme@gmail.com>
2018-05-23 12:08:43 +0200

    add hello exercise

---
+++ 02-variables/hello.php
@@ -0,0 +1,14 @@
+
+<?php
+
+// Créer une variable avec le prénom et l'afficher dasn un h1.
+//2 méthodes
+
+$prenom = 'Raphaële';
+
+//concaténation :
+echo '<h1>Hello ' . $prenom . ' !</h1>';
+
+//interpolation :
+echo "<h1>Hello $prenom !</h1>";
+
